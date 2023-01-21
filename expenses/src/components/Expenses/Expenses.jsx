3eed8cc6baeb022c96e29b64a9b897b088362883
sebastianiv21 <seluti98@gmail.com>
2023-01-21 15:52:16 -0500

    chore: expenses project and base course-goals project

---
+++ expenses/src/components/Expenses/Expenses.jsx
@@ -0,0 +1,33 @@
+import { useState } from 'react';
+import './Expenses.css';
+import Card from '../UI/Card';
+import ExpensesFilter from './ExpensesFilter';
+import ExpensesList from './ExpensesList';
+import ExpensesChart from './ExpensesChart';
+
+const Expenses = (props) => {
+  const [filteredYear, setFilteredYear] = useState('2020');
+
+  const filterChangeHandler = (selectedYear) => {
+    setFilteredYear(selectedYear);
+  };
+
+  const filteredExpenses = props.expenses.filter((expense) => {
+    return expense.date.getFullYear() == filteredYear;
+  });
+
+  return (
+    <div>
+      <Card className='expenses'>
+        <ExpensesFilter
+          selectedYear={filteredYear}
+          onChangeFilter={filterChangeHandler}
+        />
+        <ExpensesChart expenses={filteredExpenses} />
+        <ExpensesList expenses={filteredExpenses} />
+      </Card>
+    </div>
+  );
+};
+
+export default Expenses;
