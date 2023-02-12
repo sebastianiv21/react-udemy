f4ad34dd64e331cfe8a1698c2eb5e99e1ebc49cf
sebastianiv21 <seluti98@gmail.com>
2023-02-12 10:45:33 -0500

    basic redux toolkit integration

--- redux-basics/src/store/index.js
+++ redux-basics/src/store/index.js
@@ -1,59 +1,9 @@
-import { createStore } from 'redux';
-import { createSlice } from '@reduxjs/toolkit';
+import { configureStore } from '@reduxjs/toolkit';
+import counterReducer from './counterSlice';
+import authReducer from './authSlice';
 
-const initialState = { counter: 0, showCounter: true };
-
-const counterSlice = createSlice({
-  name: 'counter',
-  initialState,
-  reducers:{
-    incremet(state) {
-      state.counter++;
-    },
-    decrement(state) {
-      state.counter--;
-    },
-    increase(state, action) {
-      state.counter = state.counter + action.amount;
-    },
-    toggleCounter(state) {
-      state.showCounter = !state.showCounter;
-    }
-  }
+const store = configureStore({
+  reducer: { counter: counterReducer, auth: authReducer },
 });
-
-const counterReducer = (state = initialState, action) => {
-  if (action.type === 'increment') {
-    return {
-      counter: state.counter + 1,
-      showCounter: state.showCounter
-    };
-  }
-
-  if (action.type === 'increase') {
-    return {
-      counter: state.counter + action.amount,
-      showCounter: state.showCounter
-    };
-  }
-
-  if (action.type === 'decrement') {
-    return {
-      counter: state.counter - 1,
-      showCounter: state.showCounter
-    };
-  }
-
-  if (action.type === 'toggle') {
-    return {
-      counter: state.counter,
-      showCounter: !state.showCounter
-    };
-  }
-
-  return state;
-};
-
-const store = createStore(counterReducer);
 
 export default store;
