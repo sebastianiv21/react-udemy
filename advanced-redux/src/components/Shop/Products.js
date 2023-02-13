5027e7de2530a677fadaf9c25e2176f714b7bd81
sebastianiv21 <seluti98@gmail.com>
2023-02-13 10:45:21 -0500

    added redux logic for cart

--- advanced-redux/src/components/Shop/Products.js
+++ advanced-redux/src/components/Shop/Products.js
@@ -1,16 +1,47 @@
 import ProductItem from './ProductItem';
 import classes from './Products.module.css';
 
+const DUMMY_PRODUCTS = [
+  {
+    id: 'p1',
+    price: 6,
+    title: 'My first book',
+    description: 'The first book I ever wrote',
+  },
+  {
+    id: 'p2',
+    price: 4,
+    title: 'Bike',
+    description: 'A bike to ride on weekends',
+  },
+  {
+    id: 'p3',
+    price: 7,
+    title: 'Car toy',
+    description: 'The car toy for your kid',
+  },
+  {
+    id: 'p4',
+    price: 8,
+    title: 'Nails',
+    description: 'Some nails to repair the house',
+  },
+];
+
 const Products = (props) => {
   return (
     <section className={classes.products}>
       <h2>Buy your favorite products</h2>
       <ul>
-        <ProductItem
-          title='Test'
-          price={6}
-          description='This is a first product - amazing!'
-        />
+        {DUMMY_PRODUCTS.map((product) => (
+          <ProductItem
+            key={product.id}
+            id={product.id}
+            title={product.title}
+            price={product.price}
+            description={product.description}
+          />
+        ))}
       </ul>
     </section>
   );
