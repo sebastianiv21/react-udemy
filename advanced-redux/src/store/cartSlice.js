ff4ec9c0e0c7ec5dde0ba01d9e11b0657945c39e
sebastianiv21 <seluti98@gmail.com>
2023-02-14 08:31:28 -0500

    added async logic

--- advanced-redux/src/store/cartSlice.js
+++ advanced-redux/src/store/cartSlice.js
@@ -5,19 +5,21 @@ const cartSlice = createSlice({
   initialState: {
     items: [],
     totalQuantity: 0,
+    changed: false,
   },
   reducers: {
     addItemToCart(state, action) {
       const newItem = action.payload;
       const existingItem = state.items.find((item) => item.id === newItem.id);
       state.totalQuantity++;
+      state.changed = true;
       if (!existingItem) {
         state.items.push({
           id: newItem.id,
           price: newItem.price,
           quantity: 1,
           totalPrice: newItem.price,
-          name: newItem.title
+          name: newItem.title,
         });
       } else {
         existingItem.quantity = existingItem.quantity + 1;
@@ -27,15 +29,16 @@ const cartSlice = createSlice({
       }
     },
     removeItemToCart(state, action) {
-        const id = action.payload;
-        const existingItem = state.items.find(item => item.id === id);
-        state.totalQuantity--;
-        if (existingItem.quantity === 1) {
-            state.items = state.items.filter(item => item.id !== id)
-        } else {
-            existingItem.quantity--;
-            existingItem.totalPrice = existingItem.totalPrice - existingItem.price;
-        }
+      const id = action.payload;
+      const existingItem = state.items.find((item) => item.id === id);
+      state.totalQuantity--;
+      state.changed = true;
+      if (existingItem.quantity === 1) {
+        state.items = state.items.filter((item) => item.id !== id);
+      } else {
+        existingItem.quantity--;
+        existingItem.totalPrice = existingItem.totalPrice - existingItem.price;
+      }
     },
   },
 });
